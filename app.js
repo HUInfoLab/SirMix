4f17fdf89434b041e452c8f877c722596a248816
tjm5114 <tjm5114@gmail.com>
2015-09-19 11:31:52 -0400

    Launch Server

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var http = require('http');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000);
+
+//set port 
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -54,6 +59,13 @@ app.use(function(err, req, res, next) {
     message: err.message,
     error: {}
   });
+});
+
+//Fire up the web server! 
+var server = http.createServer(app);
+var port = app.get('port');
+server.listen(port, function() {
+  var address = 'http://localhost:' + port; 
 });
 
 
